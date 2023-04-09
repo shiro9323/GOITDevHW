379de2cd2922720014ddfb7ca82b4977ed083bac
AndriiSh <trancelvania93@gmail.com>
2023-04-09 19:23:24 +0300

    + new task maven jar

---
+++ src/main/java/module1Maven/Person.java
@@ -0,0 +1,19 @@
+package module1Maven;
+
+public class Person {
+    private final String name;
+    private final String lastName;
+
+    public Person(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
